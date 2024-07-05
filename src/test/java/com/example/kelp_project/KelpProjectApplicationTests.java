9f44d44b660d5fe2e1ec30b823d42327f8d9612c
Matheus Eugenio <math.eugenioti@gmail.com>
2024-07-05 11:39:12 -0300

    feat: adicionando configurações iniciais

---
+++ src/test/java/com/example/kelp_project/KelpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kelp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
