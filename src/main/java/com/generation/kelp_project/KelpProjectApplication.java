eaa792e03b7cc1311a8bebcbe8ed72a3aaed3177
Lucasgr <lucasrctt@gmail.com>
2024-07-05 12:12:04 -0300

    feat: Implementando o pacote Model e classe CategoriaModel

--- src/main/java/com/generation/kelp_project/KelpProjectApplication.java
+++ src/main/java/com/generation/kelp_project/KelpProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.kelp_project;
+package com.generation.kelp_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/generation/kelp_project/model/CategoriaModel.java
@@ -0,0 +1,53 @@
+package com.generation.kelp_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+
+public class CategoriaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "nome obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 5 e no máximo 100 caracters")
+	private String nome;
+	
+	@NotBlank(message = "descrição obrigatória")
+	@Size(min = 5, max = 100, message = "O atributo descrição deve conter no mínimo 5 e no máximo 100 caracters")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
