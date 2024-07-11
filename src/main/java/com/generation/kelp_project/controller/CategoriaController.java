37b800dd90128c68d40e8e3cd43480fd376ce322
Yasmin de França Ribeiro <64998808+yasmin-fribeiro@users.noreply.github.com>
2024-07-11 16:07:42 -0300

    chore: Refatorando método getByNome

--- src/main/java/com/generation/kelp_project/controller/CategoriaController.java
+++ src/main/java/com/generation/kelp_project/controller/CategoriaController.java
@@ -43,7 +43,7 @@ public class CategoriaController {
 	}
 
 	@GetMapping("/nome/{nome}")
-	public ResponseEntity<List<CategoriaModel>> getByNome_jogo(@PathVariable String nome) {
+	public ResponseEntity<List<CategoriaModel>> getByNome(@PathVariable String nome) {
 		return ResponseEntity.ok(categoriaRepository.findAllByNomeContainingIgnoreCase(nome));
 
 	}
